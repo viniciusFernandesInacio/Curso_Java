9d491ece944b9d021e49496abacf2190b98f8d83
vinicoloco <vinicius.inacio1997@gmail.com>
2022-05-18 18:48:16 -0300

    Nova aulas concluídas.

--- au4Scanner_input.java
+++ au4Scanner_input.java
@@ -1,22 +1,25 @@
-
-
-
 import java.util.Scanner;
 
 public class au4Scanner_input {
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
-        int n1 = 0, n2 = 0, res = 0;
-        String nome = "";
+        int n1 = 0, n2 = 0,n3 = 0,n4 = 0,media = 0;
+        final int mediaFinal = 60; //final int --> indica que a variavel é uma constante.
+        String nome = "",res = "";
 
         System.out.println("Digite o seu nome: ");
         nome = scan.nextLine();
-        System.out.println("Digite um numero: ");
+        System.out.println("Digite a primeira nota: ");
         n1 = scan.nextInt();
-        System.out.println("Digite um numero: ");
+        System.out.println("Digite a segunda nota: ");
         n2 = scan.nextInt();
-        res = n1 + n2;
-        System.out.printf("%s a soma de %d com %d e igual a %d.",nome,n1,n2,res);
+        System.out.println("Digite a terceira nota: ");
+        n3 = scan.nextInt();
+        System.out.println("Digite a quarta nota: ");
+        n4 = scan.nextInt();
+        media = (n1 + n2 + n3 + n4) / 4;
+        if(media >= mediaFinal)
+        System.out.printf("Aluno(a) %s foi %s, com a nota de %d",nome,res,media);
     }
 }
