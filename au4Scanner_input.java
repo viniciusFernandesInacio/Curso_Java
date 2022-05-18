962281514b21e7f6858e98288899d9cdf23fdf3d
vinicoloco <vinicius.inacio1997@gmail.com>
2022-05-18 08:01:34 -0300

    Alterações no código.

--- au4Scanner_input.java
+++ au4Scanner_input.java
@@ -8,12 +8,15 @@ public class au4Scanner_input {
         
         Scanner scan = new Scanner(System.in);
         int n1 = 0, n2 = 0, res = 0;
+        String nome = "";
 
-        System.out.println("Digite um número: ");
+        System.out.println("Digite o seu nome: ");
+        nome = scan.nextLine();
+        System.out.println("Digite um numero: ");
         n1 = scan.nextInt();
-        System.out.println("Digite um número: ");
+        System.out.println("Digite um numero: ");
         n2 = scan.nextInt();
         res = n1 + n2;
-        System.out.printf("A soma de %d com %d é igual a %d.",n1,n2,res);
+        System.out.printf("%s a soma de %d com %d e igual a %d.",nome,n1,n2,res);
     }
 }
