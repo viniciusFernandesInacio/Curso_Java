386a90408d8068a10fe7350d3c3aca0f0bd43b92
vinicoloco <vinicius.inacio1997@gmail.com>
2022-05-19 19:04:12 -0300

    Novas aulas concluídas.

--- au05loops.java
+++ au05loops.java
@@ -1,7 +1,30 @@
 import java.util.Scanner;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class au05loops {
     public static void main(String[] args) {
-        
-    }
+
+        Scanner scan = new Scanner(System.in);
+
+        /*FOR
+        for(int cont = 0 ; cont < 5 ; cont++) {
+            System.out.println(cont + "Biscoito");
+        }*/
+
+        /*
+        int cont = 0;
+        while(cont != 7){
+            System.out.print("Digite um numero: ");
+            cont = scan.nextInt();
+        }*/
+
+
+        int cont = 0;
+        do{
+            System.out.println(cont + "Biscoito");
+            cont++;
+        }while(cont < 7);
+        System.out.println("Fim do programa.");
+    }   
 }
