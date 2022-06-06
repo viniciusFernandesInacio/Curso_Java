f1b600645206802dc60e10cc5752f31e0807f9f3
vinicoloco <vinicius.inacio1997@gmail.com>
2022-06-06 14:38:13 -0300

    Novas aulas.

--- au06Array.java
+++ au06Array.java
@@ -6,7 +6,12 @@ public class au06Array {
        char[] gabarito={'a','a','d','b','c'}; 
        char[] respostas=new char[tam]; 
        int nota=0;
-       Scanner scan=new Scanner(System.in);
+       try (Scanner scan = new Scanner(System.in)) {
+        for(int i=0;i<tam;i++){
+            System.out.printf("Informe a %dº resposta: ",i);
+            respostas[i]=scan.nextLine().charAt(0);
+            }
+    }
 
        /*respostas[0]='a';
        respostas[1]='b';
@@ -15,11 +20,6 @@ public class au06Array {
        respostas[4]='b';*/
 
        for(int i=0;i<tam;i++){
-        System.out.printf("Informe a %dº resposta: ",i);
-        respostas[i]=scan.nextLine().charAt(0);
-        }
-
-        for(int i=0;i<tam;i++){
             if(respostas[i] == gabarito[i]){
                 nota++;
             }
--- au07ArrayPt2.java
+++ au07ArrayPt2.java
@@ -1,3 +1,29 @@
+import java.util.Arrays;
+
 public class au07ArrayPt2 {
-    
+
+    public static void main(String[] args){
+        
+        final int tam=10;
+        int[] num={60,10,6,3,15,7,9,2,10,4};
+        int[] numeros=new int[tam];
+        int p=23;
+        int pos;
+
+        //Arrays.sort(num);
+        ///Arrays.fill(numeros,tam);
+        //System.arraycopy(num, 0, numeros, 0, tam);
+        //System.out.printf("Arrays sao iguais: %s",Arrays.equals(num, numeros) ? "Sim" : "Nao");
+
+        Arrays.sort(num);
+        pos=Arrays.binarySearch(num, p);
+        System.out.printf("O elemento %d esta na array? %s",p,pos > 0 ? "Sim" : "Nao");
+        
+        /*for(int i=0;i<num.length;i++){
+            System.out.printf("%d - ",num[i]);
+        }*/
+        for(int n:numeros){
+            System.out.printf("%d - ",n);
+        }
+    }
 }
